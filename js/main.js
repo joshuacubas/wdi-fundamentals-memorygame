43d68d816d24bbe11d1a1233e8dee68eef1276ee
Joshua M Cubas <joshuacubas@yahoo.com>
2020-02-18 09:45:15 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
-console.log("Up and running!");
-let cardOne="queen";
-let cardTwo="king";
-let cardThree="queen";
-let cardFour="king";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardFour);
+//console.log("Up and running!");
+
+// console.log("User flipped" + cardOne);
+// console.log("User flipped" + cardFour);
+let cards=["queen","queen","king", "king"];
+let cardsInPlay=[];
+
+const checkForMatch = () => {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	  console.log("You found a match!");
+	} else {
+	  console.log("Sorry, try again.");
+	}
+}
+
+const flipCard = (cardId) => {
+	let cardOne = cards[0];
+	cardsInPlay.push(cardOne);
+	console.log("User flipped " + cardOne);
+
+	let cardTwo = cards[2];
+	cardsInPlay.push(cardTwo);
+	console.log("User flipped " + cardTwo);
+	checkForMatch()
+}
+
+flipCard(0);
+flipCard(2);
